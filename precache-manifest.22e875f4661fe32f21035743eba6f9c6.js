02f444dc53a76f52df610f96d7636ee363c35027
Fredrik Rönnehag <fredrik.ronnehag@gmail.com>
2019-04-11 10:56:02 +0200

    Updates

--- precache-manifest.22e875f4661fe32f21035743eba6f9c6.js
+++ precache-manifest.22e875f4661fe32f21035743eba6f9c6.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Ronnehag/reactjs-memory-game/static/js/runtime~main.f7a0da46.js"
   },
   {
-    "revision": "c25feffbe07ef0c0ed15",
-    "url": "/Ronnehag/reactjs-memory-game/static/js/main.cb962f1a.chunk.js"
+    "revision": "22a4fbcced01f1655819",
+    "url": "/Ronnehag/reactjs-memory-game/static/js/main.8a6f8639.chunk.js"
   },
   {
-    "revision": "0ea92328d8de60e06d17",
-    "url": "/Ronnehag/reactjs-memory-game/static/js/2.42bed1b2.chunk.js"
+    "revision": "7f8dcc5428c714b2d273",
+    "url": "/Ronnehag/reactjs-memory-game/static/js/2.679dec24.chunk.js"
   },
   {
-    "revision": "c25feffbe07ef0c0ed15",
+    "revision": "22a4fbcced01f1655819",
     "url": "/Ronnehag/reactjs-memory-game/static/css/main.729d3742.chunk.css"
   },
   {
-    "revision": "05a54111d27588afe8046c8a83697633",
+    "revision": "9072a645ec68bce14b39c6a1913635cd",
     "url": "/Ronnehag/reactjs-memory-game/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Ronnehag/reactjs-memory-game/precache-manifest.ac758f015266c23b93140910504ec321.js"
+  "/Ronnehag/reactjs-memory-game/precache-manifest.22e875f4661fe32f21035743eba6f9c6.js"
 );
 
 workbox.clientsClaim();
