25b1de76ce85833a1485514096adfeb819d7b5a8
Fredrik Rönnehag <fredrik.ronnehag@gmail.com>
2019-04-11 10:58:38 +0200

    fixes to routing

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 ReactDOM.render(
     <Router>
-        <Route exact path="/" component={App} />
+        <Switch>
+            <Route exact path="/" component={App} />
+        </Switch>
     </Router>, document.getElementById('root'));
